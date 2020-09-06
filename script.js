d336e69a1597112d7a2e201fbf19a72c299a3ef8
egorvegel <48829138+egorvegel@users.noreply.github.com>
2020-09-06 13:46:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 $(function(){
     /*  Кнопка*/
-    let btUp = $('.btnUp');
-    $(btUp).on('click',  function(evt){
+    let btnUp = $('.btnUp');
+    $(btnUp).on('click',  function(evt){
         evt.preventDefault();
-        console.log(this);
+
         $('html').animate({
             scrollTop: 0,
         }, 300)
     })
 
     function checkDis(){
-        if(this.scrollY > 870){
-            $(btUp).show(300);
+        console.log($(this).scrollTop())
+        if($(this).scrollTop() > 800){
+            $(btnUp).css('opacity', 1);
         }else{
-            $(btUp).hide(300);
+            $(btnUp).css('opacity', 0);
         }
     }
     checkDis();
     $(window).on('scroll',  function(){
         checkDis();
     })
-    
+
     /* Слайдер */
     $('.slick_wrap').css('position', 'relative').slick({
         dots: true,
         prevArrow: '<button id="prev" type="button" class="btn slick-prev"></button>',
         nextArrow: '<button id="next" type="button" class="btn slick-next"></button>',
-        
-        adaptiveHeight: true
     });
-    
+
     /* Мобильное меню */
     let menu = $('.nav_menu');
     $('.nav--btn').on('click', function(){
        if($(menu).css('display') == 'none'){
             $(menu).show(300);
+
         }else{
             $(menu).hide(300);
-        }    
+            menu.css('display', 'block');
+        }
     })
-    $(window).resize(function() {
+
+   $(window).resize(function() {
         if($(this).width() > 800){
             menu.css('display', 'block');
+        }else{
+            menu.css('display', 'none');
         }
     });
 
@@ -53,6 +57,5 @@ $(function(){
         $('html').animate({
             scrollTop: elem.offset().top - 100,
         }, 500)
-        $(menu).hide(300);
     })
 })
